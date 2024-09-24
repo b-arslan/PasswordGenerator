3261aaffc9f320f99e8644e0ead2628c483302fd
Bugra Arslan <bugraarslan5@outlook.com>
2024-09-24 12:21:46 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { useState } from 'react';
+import styles from './styles/page.module.scss';
+import { Layout, Row, Col, Button, Input, message } from 'antd';
+import { CopyOutlined } from '@ant-design/icons';
+import { validateHeaderName } from 'http';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const { Content } = Layout;
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const Home = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleGenerate = () => {
+        setLoading(true);
+        
+        setTimeout(() => {
+            setInputValue('bAdnit-zeqsam-qykpi4');
+        }, 1000);
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }
+
+    const handleInputChange = (e: any) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleClipboard = () => {
+        navigator.clipboard.writeText(inputValue);
+        message.success('Password Copied!');
+    }
+
+    return (
+
+        <Layout className={styles.layout}>
+            <Content className={styles.content}>
+                <Row style={{height: '100%', width: '100%'}}>
+                    <Col span={24} className={styles.col} style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                        <div className={styles.myDiv}>
+                            <div style={{textAlign: 'center'}}>
+                                <h1 className={styles.title}>Password Generator</h1>
+                                <h4 className={styles.desc}>With an AI powered system, you can generate unique and strong passwords</h4>
+                            </div>
+                            <Button type='primary' className={styles.btn} loading={loading} onClick={handleGenerate}>Generate</Button>
+                            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '1rem'}}>
+                                <Input value={inputValue} disabled placeholder='generated password' onChange={handleInputChange}  id='passwordInput' className={styles.inputPass} /> <CopyOutlined onClick={handleClipboard} style={{fontSize: '24px', color: '#000', cursor: 'pointer'}}/>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
+
+    );
 }
+
+export default Home;
